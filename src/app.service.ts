02bcc4283a6d2eb045b9e524ee4e516085a6af90
Tinh Ha <tinh.ha@officeatwork.com>
2025-08-12 10:01:07 +0700

    update data for testing ci-cd

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! this is updated by Tinh Ha';
+    return 'Hello World! this is updated by Tinh Ha 1';
   }
 }
